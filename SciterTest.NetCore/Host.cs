f3aa7133d79631188ab6f4ce229c2d9bf1058632
wdcossey <wdcossey@github.com>
2020-04-07 17:52:38 +0100

    Added missing files

---
+++ SciterTest.NetCore/Host.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using SciterCore;
+using SciterCore.Interop;
+using SciterValue = SciterCore.SciterValue;
+
+namespace SciterTest.NetCore
+{
+	public class Host : BaseHost
+	{
+		public Host(SciterWindow wnd)
+		{
+			var host = this;
+			host.Setup(wnd);
+			host.AttachEvh(new HostEvh());
+			host.SetupPage("index.html");
+			wnd.Show();
+		}
+
+		// Things to do here:
+		// -override OnLoadData() to customize or track resource loading
+		// -override OnPostedNotification() to handle notifications generated with SciterHost.PostNotification()
+	}
+
+	public class HostEvh : SciterEventHandler
+	{
+		// A dynamic script call handler. Any call in TIScript to function 'view.Host_HelloWorld()' with invoke this method
+		// Notice that signature of these handlers is always the same
+		// (Hint: install OmniCode snippets which adds the 'ssh' snippet to C# editor so you can easily declare 'Siter Handler' methods)
+		// (see: https://github.com/MISoftware/OmniCode-Snippets)
+		public bool Host_HelloWorld(SciterElement el, SciterValue[] args, out SciterValue result)
+		{
+			result = new SciterValue("Hello World! (from native side)");
+			return true;
+		}
+
+		// (Hint: to overload C# methods of SciterEventHandler base class, type 'override', press space, and VS/Xamarin will suggest the methods you can override)
+	}
+
+	// This base class overrides OnLoadData and does the resource loading strategy
+	// explained at http://misoftware.rs/Bootstrap/Dev
+	//
+	// - in DEBUG mode: resources loaded directly from the file system
+	// - in RELEASE mode: resources loaded from by a SciterArchive (packed binary data contained as C# code in ArchiveResource.cs)
+	public class BaseHost : SciterHost
+	{
+		protected static Sciter.SciterApi _api = Sciter.Api;
+		protected SciterArchive _archive = new SciterArchive();
+		protected SciterWindow _wnd;
+
+		public BaseHost()
+		{
+		#if !DEBUG
+			_archive.Open(SciterAppResource.ArchiveResource.resources);
+		#endif
+		}
+
+		public void Setup(SciterWindow wnd)
+		{
+			_wnd = wnd;
+			SetupWindow(wnd);
+		}
+
+		public void SetupPage(string page_from_res_folder)
+		{
+		#if DEBUG
+			string rootPath = Path.GetDirectoryName( Assembly.GetExecutingAssembly().Location ).Replace('\\', '/');
+
+			#if OSX
+			rootPath = rootPath + "/../../../../..";
+			#else
+
+            rootPath = rootPath + "/../../..";
+			#endif
+
+			string path = rootPath + "/res/" + page_from_res_folder;
+			Debug.Assert(File.Exists(path));
+
+			string url = "file://" + path;
+		#else
+			string url = "archive://app/" + page_from_res_folder;
+		#endif
+
+			bool res = _wnd.LoadPage(url);
+			Debug.Assert(res);
+		}
+
+		protected override SciterXDef.LoadResult OnLoadData(SciterXDef.SCN_LOAD_DATA sld)
+		{
+			if(sld.uri.StartsWith("archive://app/"))
+			{
+				// load resource from SciterArchive
+				string path = sld.uri.Substring(14);
+				byte[] data = _archive.Get(path);
+				if(data!=null)
+					_api.SciterDataReady(_wnd._hwnd, sld.uri, data, (uint) data.Length);
+			}
+
+			// call base to ensure LibConsole is loaded
+			return base.OnLoadData(sld);
+		}
+	}
+}
---
+++ SciterTest.NetCore/Window.cs
@@ -0,0 +1,25 @@
+using SciterCore;
+using SciterCore.Interop;
+
+namespace SciterTest.NetCore
+{
+	public class Window : SciterWindow
+	{
+		public Window()
+		{
+			var wnd = this;
+            wnd.CreateMainWindow(800, 600,
+                SciterXDef.SCITER_CREATE_WINDOW_FLAGS.SW_MAIN |
+                SciterXDef.SCITER_CREATE_WINDOW_FLAGS.SW_CONTROLS |
+                SciterXDef.SCITER_CREATE_WINDOW_FLAGS.SW_TITLEBAR |
+                //SciterXDef.SCITER_CREATE_WINDOW_FLAGS.SW_GLASSY |
+                SciterXDef.SCITER_CREATE_WINDOW_FLAGS.SW_ENABLE_DEBUG |
+                SciterXDef.SCITER_CREATE_WINDOW_FLAGS.SW_RESIZEABLE);
+			wnd.CenterTopLevelWindow();
+			wnd.Title = "SciterTest.Gtk";
+			#if WINDOWS
+			wnd.Icon = Properties.Resources.IconMain;
+			#endif
+        }
+	}
+}
