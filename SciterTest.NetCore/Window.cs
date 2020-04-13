ed231470cedcd3eafdf5412aad755d2ba6ed9de2
wdcossey <wdcossey@github.com>
2020-04-13 15:01:50 +0100

    Updated .Net Core Sample (for Windows)

--- SciterTest.NetCore/Window.cs
+++ SciterTest.NetCore/Window.cs
@@ -8,15 +8,9 @@ namespace SciterTest.NetCore
 		public Window()
 		{
 			var wnd = this;
-            wnd.CreateMainWindow(800, 600,
-                SciterXDef.SCITER_CREATE_WINDOW_FLAGS.SW_MAIN |
-                SciterXDef.SCITER_CREATE_WINDOW_FLAGS.SW_CONTROLS |
-                SciterXDef.SCITER_CREATE_WINDOW_FLAGS.SW_TITLEBAR |
-                //SciterXDef.SCITER_CREATE_WINDOW_FLAGS.SW_GLASSY |
-                SciterXDef.SCITER_CREATE_WINDOW_FLAGS.SW_ENABLE_DEBUG |
-                SciterXDef.SCITER_CREATE_WINDOW_FLAGS.SW_RESIZEABLE);
-			wnd.CenterTopLevelWindow();
-			wnd.Title = "SciterTest.Gtk";
+            CreateMainWindow(800, 600);
+            CenterTopLevelWindow();
+			SetTitle("SciterTest.NetCore");
 			#if WINDOWS
 			wnd.Icon = Properties.Resources.IconMain;
 			#endif
