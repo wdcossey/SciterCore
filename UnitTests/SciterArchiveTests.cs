5ee8eaa614209bbdac353805d9dbcd569b8e39cc
wdcossey <wdcossey@github.com>
2020-09-21 09:37:40 +0100

    Updated SciterArchiveTests

--- UnitTests/SciterArchiveTests.cs
+++ UnitTests/SciterArchiveTests.cs
@@ -30,7 +30,7 @@ namespace SciterCore.UnitTests
         }
 
         [Test]
-        public void Archive_open_async()
+        public void Open_archive_async()
         {
             Assert.DoesNotThrowAsync(() => _archive.OpenAsync(_assembly));
             Assert.IsTrue(_archive.IsOpen);
@@ -38,7 +38,7 @@ namespace SciterCore.UnitTests
         }
 
         [Test]
-        public void Archive_open()
+        public void Open_archive()
         {
             Assert.DoesNotThrow(() => _archive.Open(_assembly));
             Assert.IsTrue(_archive.IsOpen);
@@ -46,14 +46,14 @@ namespace SciterCore.UnitTests
         }
 
         [Test]
-        public void Archive_open_twice_throws_InvalidOperationException()
+        public void Open_archive_twice_throws_InvalidOperationException()
         {
             _archive.OpenAsync(_assembly);
             Assert.ThrowsAsync<InvalidOperationException>(() => _archive.OpenAsync(_assembly));
         }
 
         [Test]
-        public void Archive_get_valid_item()
+        public void Get_valid_item_from_archive()
         {
             byte[] buffer = null;
             
@@ -67,7 +67,7 @@ namespace SciterCore.UnitTests
         }
 
         [Test]
-        public void Archive_get_valid_item_relative_path()
+        public void Get_valid_item_relative_path()
         {
             _archive.OpenAsync(_assembly);
             var buffer = _archive.GetItem("index.html");
@@ -75,7 +75,7 @@ namespace SciterCore.UnitTests
         }
         
         [Test]
-        public void Archive_get_valid_item_relative_path_with_callback()
+        public void Get_valid_item_relative_path_with_callback()
         {
             byte[] buffer = null;
             
@@ -89,11 +89,44 @@ namespace SciterCore.UnitTests
         }
 
         [Test]
-        public void Archive_get_invalid_item()
+        public void Get_invalid_item_returns_null()
         {
             _archive.OpenAsync(_assembly);
             var buffer = _archive.GetItem(new Uri(_archive.Uri, "qwerty.html"));
             Assert.IsNull(buffer);
+        }
+
+        [Test]
+        public void Open_archive_with_a_custom_uri()
+        {
+            using (var archive = new SciterArchive(new Uri("custom://app/")))
+            {
+                archive.OpenAsync(_assembly);
+                var buffer = archive.GetItem("index.html");
+                Assert.NotNull(buffer);
+            }
+        }
+
+        [Test]
+        public void Open_archive_with_a_custom_uri_from_string()
+        {
+            using (var archive = new SciterArchive("custom://app/"))
+            {
+                archive.OpenAsync(_assembly);
+                var buffer = archive.GetItem("index.html");
+                Assert.NotNull(buffer);
+            }
+        }
+
+        [Test]
+        public void Open_archive_with_a_custom_uri_and_incorrect_item_scheme_returns_null()
+        {
+            using (var archive = new SciterArchive("custom://app/"))
+            {
+                archive.OpenAsync(_assembly);
+                var buffer = archive.GetItem("notme://app/index.html");
+                Assert.IsNull(buffer);
+            }
         }
         
     }
