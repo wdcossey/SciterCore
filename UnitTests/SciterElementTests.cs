21e09b800041f0bcb9f41dfbc07c9c62340642dc
William Cossey <will.cossey@stepstone.com>
2020-09-28 14:16:04 +0100

    Fixed some SciterElement tests

--- UnitTests/SciterElementTests.cs
+++ UnitTests/SciterElementTests.cs
@@ -16,26 +16,84 @@ namespace SciterCore.UnitTests
             Assert.AreEqual(tag, element.Tag);
         }
         
-        [Test]
-        public void Create_element_from_tag_and_text()
+        [TestCase("div")]
+        [TestCase("text")]
+        [TestCase("label")]
+        [TestCase("ul")]
+        public void Create_element_from_tag_and_text(string tag)
         {
-            var element = SciterElement.Create("div", "text");
+            var element = SciterElement.Create(tag, "text");
             Assert.IsNotNull(element);
-            Assert.AreEqual("div", element.Tag);
+            Assert.AreEqual(tag, element.Tag);
             Assert.AreEqual("text", element.Text);
-            Assert.AreEqual("<div>text</div>", element.Html);
+            Assert.AreEqual($"<{tag}>text</{tag}>", element.Html);
             Assert.AreEqual("text", element.InnerHtml);
         }
         
-        [Test]
-        [Ignore("SetText() not working!")]
-        public void Create_element_from_tag_and_set_text()
+        [TestCase("div")]
+        [TestCase("text")]
+        [TestCase("label")]
+        [TestCase("ul")]
+        public void Create_element_from_tag_and_set_text(string tag)
         {
-            var element = SciterElement.Create("div", "something");
-            element.Text = "text";
-            Assert.IsNotNull(element);
-            Assert.AreEqual("div", element.Tag);
-            Assert.AreEqual("text", element.Text);
+            SciterWindow sciterWindow = null;
+
+            try
+            {
+                sciterWindow = new SciterWindow()
+                    .CreateMainWindow(320, 240)
+                    .LoadHtml("<html></html>");
+
+                var element = sciterWindow
+                    .RootElement
+                    .AppendChildElement("body")
+                    .AppendChildElement(tag, "you should not see me!");
+                
+                element.SetText("text");
+                
+                Assert.IsNotNull(element);
+                Assert.AreEqual(tag, element.Tag);
+                Assert.AreEqual("text", element.Text);
+                Assert.AreEqual(0, element.ChildCount);
+            }
+            finally
+            {
+                sciterWindow?.Close();
+                sciterWindow?.Destroy();
+            }
+        }
+
+        [TestCase("div", "inner div text")]
+        [TestCase("text", "inner text text")]
+        [TestCase("label", "inner label text")]
+        [TestCase("ul", "inner ul text")]
+        public void Create_element_from_tag_and_set_html(string tag, string innerText)
+        {
+            SciterWindow sciterWindow = null;
+
+            try
+            {
+                sciterWindow = new SciterWindow()
+                    .CreateMainWindow(320, 240)
+                    .LoadHtml("<html></html>");
+
+                var element = sciterWindow
+                    .RootElement
+                    .AppendChildElement("body")
+                    .AppendChildElement(tag, "you should not see me!");
+
+                element.SetHtml($"<div>{innerText}</div>");
+                
+                Assert.IsNotNull(element);
+                Assert.AreEqual(tag, element.Tag);
+                Assert.AreEqual(innerText, element.Text);
+                Assert.AreEqual(1, element.ChildCount);
+            }
+            finally
+            {
+                sciterWindow?.Close();
+                sciterWindow?.Destroy();
+            }
         }
         
         [Test]
